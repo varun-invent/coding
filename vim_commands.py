291434405b989b0f9cefc6264f6dd01195d2d09a
varun.invent <varun@LAPTOP-S3EJ1EB0.localdomain>
2022-01-15 22:47:50 +0530

    Vim Commands

---
+++ vim_commands.py
@@ -0,0 +1,3 @@
+See Previous/Next vim commands: :ctrl + p/n
+Set a vertical line at 80 spaces: :set colorcolumn=80
+Forward/backward indent a block of lines: :shift+v <ArrowKeys to select the lines> >/<
