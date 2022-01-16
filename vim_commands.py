fe1667d9fe9e551b31e438e92e2a4d0d6bbcfb7e
varun.invent <varun@LAPTOP-S3EJ1EB0.localdomain>
2022-01-16 16:25:19 +0530

    Added mode commands

--- vim_commands.py
+++ vim_commands.py
@@ -1,3 +1,4 @@
 See Previous/Next vim commands: :ctrl + p/n
 Set a vertical line at 80 spaces: :set colorcolumn=80
 Forward/backward indent a block of lines: :shift+v <ArrowKeys to select the lines> >/<
+Copy/Cut and Paste: shift+v <ArrowKeys to select the lines> y(yank or copy) / d(cut) and p (paste)
